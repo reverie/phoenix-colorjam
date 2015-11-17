be56f98dc26ec86ace2971c9655f0544a570e559
Andrew Badr <andrewbadr+git@gmail.com>
2015-11-17 10:34:15 -0500

    Use production config from Heroku ENV variables

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :my_first_channel, MyFirstChannel.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "colorjam.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :my_first_channel, MyFirstChannel.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +64,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
